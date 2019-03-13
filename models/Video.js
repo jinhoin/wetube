b9a62ad556d64cbc92c4a6086c390f57d401148c
Jin ho in <in362@naver.com>
2019-03-13 22:57:51 +0900

    HomeController finished

--- models/Video.js
+++ models/Video.js
@@ -1,6 +1,6 @@
-import {Mongoose} from 'mongoose';
+import mongoose from 'mongoose';
 
-const VideoSchema = new Mongoose.Schema({
+const VideoSchema = new mongoose.Schema({
     fileUrl: {
         type: String,
         required: "File URL is required"
